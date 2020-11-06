ea387d7e89d26c04aae731d0f99a6dd5ffb8690c
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2020-11-06 03:40:26 +0100

    implemented location permissions and logic

--- app/src/main/java/be/ucll/localhistory/MapsActivity.java
+++ app/src/main/java/be/ucll/localhistory/MapsActivity.java
@@ -1,8 +1,18 @@
 package be.ucll.localhistory;
 
-import androidx.fragment.app.FragmentActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,37 +20,114 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import be.ucll.localhistory.PermissionUtils.PermissionStatus;
+
+public class MapsActivity extends AppCompatActivity
+        implements
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        // get SupportMapFragment and execute onMapReady when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    // TODO: better program flow to make things more clear and to reduce redundancy
+    @SuppressLint("MissingPermission")
+    private void enableLocation(boolean overrideRationale) {
+        if (PermissionUtils.getPermissionStatus(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PermissionStatus.GRANTED) {
+            if ((mMap != null) || (locationManager == null)) {
+                locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMapToolbarEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+            }
+        } else {
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, overrideRationale);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        enableLocation(false);
+        createMyLocationButtonListener();
+
+        if (locationManager == null) {
+            LatLng genk = new LatLng(50.96667, 5.5);
+            mMap.addMarker(new MarkerOptions().position(genk).title("Marker in Genk"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(genk, 16.0f));
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            switch (PermissionUtils.getPermissionStatus(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                case GRANTED:
+                    Toast.makeText(this, R.string.location_permission_granted, Toast.LENGTH_SHORT).show();
+                    enableLocation(false);
+                    // TODO: doesn't work because last location is not yet available immediately after enabling location
+                    jumpToMyLocation();
+                    break;
+                case DENIED:
+                    Toast.makeText(this, R.string.location_permission_declined, Toast.LENGTH_SHORT).show();
+                    break;
+                case DONT_ASK_OR_NEVER_ASKED:
+                    // never asked again in this case
+                    Toast.makeText(this, R.string.location_permission_never, Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void jumpToLocation(Location location, float zoom) {
+        LatLng latlng = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng, zoom));
+    }
+
+    @SuppressLint("MissingPermission")
+    private void jumpToMyLocation() {
+        if (locationManager != null) {
+            Criteria criteria = new Criteria();
+            Location myLocation = locationManager.getLastKnownLocation(
+                    locationManager.getBestProvider(criteria, false));
+            jumpToLocation(myLocation, 16.0f);
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    private void createMyLocationButtonListener(){
+        FloatingActionButton myLocationButton = findViewById(R.id.myLocationButton);
+        myLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (PermissionUtils.getPermissionStatus(MapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PermissionStatus.GRANTED) {
+                    jumpToMyLocation();
+                } else {
+                    enableLocation(true);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/be/ucll/localhistory/PermissionUtils.java
@@ -0,0 +1,40 @@
+package be.ucll.localhistory;
+
+import android.content.pm.PackageManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+
+
+public abstract class PermissionUtils {
+
+    public enum PermissionStatus {
+        GRANTED,
+        DENIED,
+        DONT_ASK_OR_NEVER_ASKED, // if never ask again was selected or never asked before
+        UNKNOWN,
+    }
+
+    public static void requestPermission(AppCompatActivity activity, int requestId,
+                                         String permission, boolean overrideRationale) {
+        // only request permission if overrideRationale is true or if never asked permission before
+        // (will also ask if never ask again was selected but request will be denied automatically)
+        if (overrideRationale ||
+                (getPermissionStatus(activity, permission) == PermissionStatus.DONT_ASK_OR_NEVER_ASKED)) {
+            ActivityCompat.requestPermissions(activity, new String[]{permission}, requestId);
+        }
+    }
+
+    public static PermissionStatus getPermissionStatus(AppCompatActivity activity, String permission) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+            return PermissionStatus.DENIED;
+        } else {
+            if (ActivityCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED) {
+                return PermissionStatus.GRANTED;
+            } else {
+                return PermissionStatus.DONT_ASK_OR_NEVER_ASKED;
+            }
+        }
+    }
+}
