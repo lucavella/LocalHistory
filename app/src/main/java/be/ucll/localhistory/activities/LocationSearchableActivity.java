be01b863c367908e806532a8da3ca9ac6774598c
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2021-01-03 18:55:03 +0100

    working search bar in location searchable activity

--- app/src/main/java/be/ucll/localhistory/activities/LocationSearchableActivity.java
+++ app/src/main/java/be/ucll/localhistory/activities/LocationSearchableActivity.java
@@ -1,14 +1,17 @@
 package be.ucll.localhistory.activities;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +36,79 @@ public class LocationSearchableActivity extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         handleIntent(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_maps_menu, menu);
+
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.location_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        final LocationSearchAdapter suggestionsAdapter = new LocationSearchAdapter(
+                this,
+                R.layout.location_search_suggestion_item
+        );
+
+        final SearchView searchView =
+                (SearchView) menu.findItem(R.id.location_search).getActionView();
+        searchView.setSuggestionsAdapter(suggestionsAdapter);
+        searchView.setIconifiedByDefault(false);
+
+        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+            @Override
+            public boolean onSuggestionSelect(int position) {
+                return true;
+            }
+
+            @Override
+            public boolean onSuggestionClick(int position) {
+                searchView.clearFocus();
+                Cursor selectedCursor = (Cursor) suggestionsAdapter.getItem(position);
+                int dbIdColPos = selectedCursor.getColumnIndex("db_id");
+                String dbId = selectedCursor.getString(dbIdColPos);
+
+                Uri dbIdUri = new Uri.Builder()
+                        .appendPath(getString(R.string.db_location_txt))
+                        .appendPath(dbId)
+                        .build();
+
+                Intent showLocationIntent = new Intent()
+                        .setAction(Intent.ACTION_VIEW)
+                        .setData(dbIdUri);
+
+                setResult(RESULT_OK, showLocationIntent);
+                finish();
+                return true;
+            }
+        });
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                suggestionsAdapter.UpdateAdapterCursorByQuery(newText);
+                return false;
+            }
+        });
+
+        return true;
     }
 
     private void handleIntent(Intent intent) {
