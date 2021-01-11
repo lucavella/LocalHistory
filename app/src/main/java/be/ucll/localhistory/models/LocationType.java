d639b7fa0b786f56200a522b22bfc33bce23bba0
Luca Vanderlinden <luca.vanderlinden@student.ucll.be>
2021-01-11 02:58:50 +0100

    location type to string implemented

--- app/src/main/java/be/ucll/localhistory/models/LocationType.java
+++ app/src/main/java/be/ucll/localhistory/models/LocationType.java
@@ -5,5 +5,13 @@ public enum LocationType {
     NATURE,
     MEMORIAL,
     MONUMENT,
-    BUILDING,
+    BUILDING;
+
+    @Override
+    public String toString() {
+        String first = this.name().substring(0, 1).toUpperCase();
+        String next = this.name().substring(1).toLowerCase();
+
+        return String.format("%s%s", first, next);
+    }
 }
